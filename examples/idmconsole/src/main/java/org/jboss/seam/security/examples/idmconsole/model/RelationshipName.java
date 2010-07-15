2acd851dbd3599401e47b613b74ada5b2f6ad6df
Shane Bryzak <sbryzak@redhat.com>
2010-07-15 12:28:14 +0000

    oops, will be needing this

---
+++ examples/idmconsole/src/main/java/org/jboss/seam/security/examples/idmconsole/model/RelationshipName.java
@@ -0,0 +1,42 @@
+package org.jboss.seam.security.examples.idmconsole.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * This is a simple lookup table containing relationship (i.e. role) names.
+ * 
+ * @author Shane Bryzak
+ */
+@Entity
+public class RelationshipName implements Serializable
+{
+   private static final long serialVersionUID = 8775236263787825703L;
+   
+   private Long id;
+   private String name;
+   
+   @Id @GeneratedValue
+   public Long getId()
+   {
+      return id;
+   }
+   
+   public void setId(Long id)
+   {
+      this.id = id;
+   }
+   
+   public String getName()
+   {
+      return name;
+   }
+   
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+}
