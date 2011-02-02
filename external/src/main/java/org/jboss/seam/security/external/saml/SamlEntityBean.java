923149a1777c714f1f64d409e72f99e578d11bd9
Shane Bryzak <sbryzak@gmail.com>
2011-02-03 09:12:48 +1000

    minor

--- external/src/main/java/org/jboss/seam/security/external/saml/SamlEntityBean.java
+++ external/src/main/java/org/jboss/seam/security/external/saml/SamlEntityBean.java
@@ -66,7 +66,7 @@ public abstract class SamlEntityBean extends EntityBean implements SamlEntityCon
 
    private SamlBinding preferredBinding = SamlBinding.HTTP_Post;
 
-   //@Inject
+   @Inject
    private ServletContext servletContext;
 
    @Inject
