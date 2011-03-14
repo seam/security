69338e6d40f1450b961bb92a4b805bf775f24abe
Shane Bryzak <sbryzak@gmail.com>
2011-03-15 08:31:44 +1000

    exception handling

--- examples/authorization/src/main/java/org/jboss/seam/security/examples/authorization/exception/ExceptionHandler.java
+++ examples/authorization/src/main/java/org/jboss/seam/security/examples/authorization/exception/ExceptionHandler.java
@@ -7,6 +7,9 @@ import javax.inject.Inject;
 import org.jboss.seam.exception.control.CaughtException;
 import org.jboss.seam.exception.control.Handles;
 import org.jboss.seam.exception.control.HandlesExceptions;
+import org.jboss.seam.exception.filter.StackFrame;
+import org.jboss.seam.exception.filter.StackFrameFilter;
+import org.jboss.seam.exception.filter.StackFrameFilterResult;
 import org.jboss.seam.security.AuthorizationException;
 
 /**
@@ -20,10 +23,19 @@ public class ExceptionHandler
 {
    @Inject FacesContext facesContext;
    
+   final StackFrameFilter<AuthorizationException> filter = new StackFrameFilter<AuthorizationException>() {
+      @Override
+      public StackFrameFilterResult process(StackFrame frame) {
+          return StackFrameFilterResult.TERMINATE_AFTER;
+      }
+  };
+   
    public void handleAuthorizationException(@Handles CaughtException<AuthorizationException> evt)
    {
       facesContext.addMessage(null, new FacesMessage(
             "You do not have the necessary permissions to perform that operation"));
-      evt.markHandled();
+      evt.handled();
+      
+      //filter.process(evt.getExceptionStack());
    }
 }
