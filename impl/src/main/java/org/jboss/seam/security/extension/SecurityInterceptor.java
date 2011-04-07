37eab7e5167c6d28cb4f776ff61dfa51063dab8b
Shane Bryzak <sbryzak@gmail.com>
2011-04-07 16:52:00 +1000

    removed unnecessary fields

--- impl/src/main/java/org/jboss/seam/security/extension/SecurityInterceptor.java
+++ impl/src/main/java/org/jboss/seam/security/extension/SecurityInterceptor.java
@@ -2,15 +2,12 @@ package org.jboss.seam.security.extension;
 
 import java.io.Serializable;
 import java.lang.reflect.Method;
-import java.lang.reflect.Type;
 
-import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 
-import org.jboss.seam.security.Identity;
 import org.jboss.seam.security.extension.SecurityExtension.Authorizer;
 
 /**
@@ -22,9 +19,6 @@ import org.jboss.seam.security.extension.SecurityExtension.Authorizer;
 public class SecurityInterceptor implements Serializable
 {
    private static final long serialVersionUID = -6567750187000766925L;
-   
-   @Inject BeanManager manager;
-   @Inject Identity identity;
    
    @Inject SecurityExtension extension;
    
