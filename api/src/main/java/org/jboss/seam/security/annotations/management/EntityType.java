d7659efe046076069dcecd0fbeb9c508581d25cc
Shane Bryzak <sbryzak@gmail.com>
2011-09-14 10:28:42 -0400

    support annotation-based configuration of JpaIdentityStore entity classes

---
+++ api/src/main/java/org/jboss/seam/security/annotations/management/EntityType.java
@@ -0,0 +1,5 @@
+package org.jboss.seam.security.annotations.management;
+
+public enum EntityType {
+    IDENTITY_OBJECT, IDENTITY_CREDENTIAL, IDENTITY_RELATIONSHIP, IDENTITY_ATTRIBUTE, IDENTITY_ROLE_NAME 
+}
--- impl/src/main/java/org/jboss/seam/security/management/picketlink/JpaIdentityStoreConfiguration.java
+++ impl/src/main/java/org/jboss/seam/security/management/picketlink/JpaIdentityStoreConfiguration.java
@@ -5,33 +5,78 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AnnotatedMethod;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import javax.persistence.Entity;
+
+import org.jboss.seam.security.annotations.management.EntityType;
+import org.jboss.seam.security.annotations.management.IdentityEntity;
+import org.jboss.seam.security.annotations.management.IdentityProperty;
+import org.jboss.seam.security.annotations.management.PropertyType;
+import org.jboss.seam.solder.reflection.annotated.AnnotatedTypeBuilder;
 import org.picketlink.idm.impl.configuration.metadata.IdentityStoreConfigurationMetaDataImpl;
 
 /**
  * A convenience class for setting JpaIdentityStore configuration options.
- *
+ * 
  * @author Shane Bryzak
  */
-public class JpaIdentityStoreConfiguration extends IdentityStoreConfiguration {
+@ApplicationScoped
+public class JpaIdentityStoreConfiguration extends IdentityStoreConfiguration implements Extension {
+
     private Class<?> identityClass;
     private Class<?> credentialClass;
     private Class<?> relationshipClass;
     private Class<?> roleTypeClass;
     private Class<?> attributeClass;
 
+    public <X> void processAnnotatedType(@Observes ProcessAnnotatedType<X> event,
+            final BeanManager beanManager) {
+        
+        if (event.getAnnotatedType().isAnnotationPresent(Entity.class)) {
+            AnnotatedType<X> type = event.getAnnotatedType();
+            
+            if (type.isAnnotationPresent(IdentityEntity.class)) {
+                IdentityEntity a = type.getAnnotation(IdentityEntity.class);
+                
+                switch(a.value()) { 
+                    case IDENTITY_OBJECT: 
+                        identityClass = type.getJavaClass();
+                        break;
+                    case IDENTITY_CREDENTIAL:
+                        credentialClass = type.getJavaClass();
+                        break;
+                    case IDENTITY_RELATIONSHIP: 
+                        relationshipClass = type.getJavaClass();
+                        break;
+                    case IDENTITY_ATTRIBUTE:
+                        attributeClass = type.getJavaClass();
+                        break;
+                    case IDENTITY_ROLE_NAME:
+                        roleTypeClass = type.getJavaClass();
+                        break;   
+                }
+            }
+        }        
+    }
+    
+    
     @Override
     public String getId() {
         return (super.getId() == null) ? "jpa" : super.getId();
     }
 
     /**
-     * If the identityStoreClass hasn't been set, then return JpaIdentityStore
-     * by default.
+     * If the identityStoreClass hasn't been set, then return JpaIdentityStore by default.
      */
     @Override
     public Class<?> getIdentityStoreClass() {
-        return (super.getIdentityStoreClass() == null) ?
-                JpaIdentityStore.class : super.getIdentityStoreClass();
+        return (super.getIdentityStoreClass() == null) ? JpaIdentityStore.class : super.getIdentityStoreClass();
     }
 
     public Class<?> getIdentityClass() {
@@ -78,28 +123,23 @@ public class JpaIdentityStoreConfiguration extends IdentityStoreConfiguration {
         Map<String, List<String>> options = new HashMap<String, List<String>>();
 
         if (identityClass != null) {
-            options.put(JpaIdentityStore.OPTION_IDENTITY_CLASS_NAME,
-                    createOptionList(identityClass.getName()));
+            options.put(JpaIdentityStore.OPTION_IDENTITY_CLASS_NAME, createOptionList(identityClass.getName()));
         }
 
         if (credentialClass != null) {
-            options.put(JpaIdentityStore.OPTION_CREDENTIAL_CLASS_NAME,
-                    createOptionList(credentialClass.getName()));
+            options.put(JpaIdentityStore.OPTION_CREDENTIAL_CLASS_NAME, createOptionList(credentialClass.getName()));
         }
 
         if (relationshipClass != null) {
-            options.put(JpaIdentityStore.OPTION_RELATIONSHIP_CLASS_NAME,
-                    createOptionList(relationshipClass.getName()));
+            options.put(JpaIdentityStore.OPTION_RELATIONSHIP_CLASS_NAME, createOptionList(relationshipClass.getName()));
         }
 
         if (roleTypeClass != null) {
-            options.put(JpaIdentityStore.OPTION_ROLE_TYPE_CLASS_NAME,
-                    createOptionList(roleTypeClass.getName()));
+            options.put(JpaIdentityStore.OPTION_ROLE_TYPE_CLASS_NAME, createOptionList(roleTypeClass.getName()));
         }
 
         if (attributeClass != null) {
-            options.put(JpaIdentityStore.OPTION_ATTRIBUTE_CLASS_NAME,
-                    createOptionList(attributeClass.getName()));
+            options.put(JpaIdentityStore.OPTION_ATTRIBUTE_CLASS_NAME, createOptionList(attributeClass.getName()));
         }
 
         store.setOptions(options);
@@ -111,7 +151,8 @@ public class JpaIdentityStoreConfiguration extends IdentityStoreConfiguration {
 
     private List<String> createOptionList(String... values) {
         List<String> vals = new ArrayList<String>();
-        for (String v : values) vals.add(v);
+        for (String v : values)
+            vals.add(v);
         return vals;
     }
 }
