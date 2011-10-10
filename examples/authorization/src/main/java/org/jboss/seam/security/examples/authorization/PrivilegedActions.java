e46206adc327918baa8a16f739ea86681d6181d0
Shane Bryzak <sbryzak@gmail.com>
2011-10-10 19:59:39 +1000

    fix some deployment issues

--- examples/authorization/src/main/java/org/jboss/seam/security/examples/authorization/PrivilegedActions.java
+++ examples/authorization/src/main/java/org/jboss/seam/security/examples/authorization/PrivilegedActions.java
@@ -1,10 +1,9 @@
 package org.jboss.seam.security.examples.authorization;
 
 import javax.enterprise.inject.Model;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
+import org.jboss.seam.international.status.Messages;
 import org.jboss.seam.security.annotations.LoggedIn;
 import org.jboss.seam.security.examples.authorization.annotations.Admin;
 import org.jboss.seam.security.examples.authorization.annotations.Foo;
@@ -16,31 +15,30 @@ import org.jboss.seam.security.examples.authorization.annotations.User;
 public
 @Model
 class PrivilegedActions {
-    @Inject
-    FacesContext facesContext;
+    @Inject Messages messages;
 
     @Admin
     public void doSomethingRestricted() {
-        facesContext.addMessage(null, new FacesMessage("doSomethingRestricted() invoked"));
+        messages.info("doSomethingRestricted() invoked");
     }
 
     @Foo(bar = "abc", zzz = "nonbindingvalue")
     public void doFooAbc() {
-        facesContext.addMessage(null, new FacesMessage("doFooAbc() invoked"));
+        messages.info("doFooAbc() invoked");
     }
 
     @Foo(bar = "def")
     public void doFooDef() {
-        facesContext.addMessage(null, new FacesMessage("doFooDef() invoked"));
+        messages.info("doFooDef() invoked");
     }
 
     @LoggedIn
     public void doLoggedIn() {
-        facesContext.addMessage(null, new FacesMessage("doLoggedIn() invoked"));
+        messages.info("doLoggedIn() invoked");
     }
 
     @User
     public void doUserAction() {
-        facesContext.addMessage(null, new FacesMessage("doUserAction() invoked"));
+        messages.info("doUserAction() invoked");
     }
 }
--- examples/authorization/src/main/java/org/jboss/seam/security/examples/authorization/exception/ExceptionHandler.java
+++ examples/authorization/src/main/java/org/jboss/seam/security/examples/authorization/exception/ExceptionHandler.java
@@ -1,13 +1,12 @@
 package org.jboss.seam.security.examples.authorization.exception;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
+import org.jboss.seam.international.status.Messages;
+import org.jboss.seam.security.AuthorizationException;
 import org.jboss.solder.exception.control.CaughtException;
 import org.jboss.solder.exception.control.Handles;
 import org.jboss.solder.exception.control.HandlesExceptions;
-import org.jboss.seam.security.AuthorizationException;
 
 /**
  * Handles user authorization exceptions
@@ -16,12 +15,10 @@ import org.jboss.seam.security.AuthorizationException;
  */
 @HandlesExceptions
 public class ExceptionHandler {
-    @Inject
-    FacesContext facesContext;
+    @Inject Messages messages;
 
     public void handleAuthorizationException(@Handles CaughtException<AuthorizationException> evt) {
-        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                "You do not have the necessary permissions to perform that operation", ""));
+        messages.error("You do not have the necessary permissions to perform that operation", "");
         evt.handled();
     }
 }
